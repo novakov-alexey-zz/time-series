f6da22c59a12cbb55383bebac4e422b176d215ae
Alexey Novakov <novakov.alex@gmail.com>
2018-10-17 10:13:15 +0200

    add main program

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,65 @@
+import java.io.{BufferedWriter, File, FileWriter}
+import java.nio.file.{Files, Paths}
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Main extends App {
+
+  override def main(args: Array[String]): Unit = {
+    args.headOption match {
+      case Some(filePath) if Files.exists(Paths.get(filePath)) => run(filePath)
+      case None => println("Please enter path to existing data input file")
+    }
+  }
+
+  private def run(filePath: String): Unit = {
+    val observations = Source.fromFile(filePath).getLines()
+      .map { l =>
+        val row = l.split("\t")
+        Observations(row(0).toLong, row(1).toDouble)
+      }
+
+    val windowSize = 60
+    val bw = new BufferedWriter(new FileWriter(new File("output.txt")))
+
+    bw.write("Time\tValue\tN_O\tRoll_Sum\tMin_Value\tMax_Value\n")
+
+    observations.foldLeft(List[Observations]() -> Stats(0, Int.MaxValue, Int.MinValue, 0)) {
+      case ((series, stats), ob) =>
+
+        @tailrec
+        def calcStats(st: Stats, ss: List[Observations]): (Stats, List[Observations]) = {
+          if (ss.isEmpty || ob.time - ss.head.time <= windowSize) {
+            val window = ss :+ ob
+            val newStats = st
+              .copy(
+                st.sum + ob.value,
+                Math.min(st.min, ob.value),
+                Math.max(st.max, ob.value),
+                window.length
+              )
+            (newStats, window)
+          }
+          else {
+            val newStats = st
+              .copy(
+                st.sum - ss.head.value,
+                if (ss.tail.nonEmpty) ss.tail.map(_.value).min else ob.value,
+                if (ss.tail.nonEmpty) ss.tail.map(_.value).max else ob.value,
+                ss.tail.length - 1
+              )
+            calcStats(newStats, ss.tail)
+          }
+        }
+
+        val (newStats, newSeries) = calcStats(stats, series)
+        bw.write(
+          f"${ob.time}\t${ob.value}\t${newStats.count}\t${newStats.sum}%.5f\t${newStats.min}%.5f\t${newStats.max}%.5f\n"
+        )
+        (newSeries, newStats)
+    }
+
+    bw.close()
+  }
+}
